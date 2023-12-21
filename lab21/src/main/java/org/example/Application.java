b0a826159e1da2002fd3b56ba11199f5e843ab27
Creepmoon <112748569+Creepmoon@users.noreply.github.com>
2023-12-21 17:45:33 +0300

    Update Application.java

--- lab21/src/main/java/org/example/Application.java
+++ lab21/src/main/java/org/example/Application.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Application {
     public static void main(final String[] args) {
+        Boxes<Beer>  beerBoxes = new Boxes<Beer>();
+        Beer beer= new Beer();
+        beer.setName("Baltika");
+        beerBoxes.put(beer);
 
+        Boxes<Shoe> shoeBoxes = new Boxes<Shoe>();
+        Shoe shoe = new Shoe();
+        shoe.setName("VOVA ADIDAS");
+        shoeBoxes.put(shoe);
     }
 }
